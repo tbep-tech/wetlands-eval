b80322b5a3160dd9f6442f024e735a7f9c81b95e
fawda123 <mbafs2012@gmail.com>
2023-06-09 07:43:31 -0400

    proc through Nevada

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,54 +1,135 @@
-library(tidyverse)
+library(dplyr)
 library(sf)
-library(rvest)
+library(datasets)
 library(usdata)
-library(nhdR)
+library(here)
+library(archive)
+library(gdalUtilities)
 
-abbrs <- state_stats$abbr
+sf_use_s2(FALSE)
 
-# wetland urls
-wetfls <- 'https://www.fws.gov/program/national-wetlands-inventory/download-state-wetlands-data' %>%
-  read_html() %>%
-  html_elements("table") %>%
-  html_elements("tr") %>%
-  html_elements("a") %>%
-  html_attr("href") %>%
-  .[grepl('shapefile', .)] %>%
-  .[gsub('(^.*)\\_shapefile.*$', '\\1', basename(.)) %in% abbrs]
+# base urls
+nwibaseurl <- 'https://documentst.ecosphere.fws.gov/wetlands/data/State-Downloads/'
+nhdbaseurl <- 'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHD/State/'
+
+# to avoid error ParseException: Unknown WKB type 12.
+ensure_multipolygons <- function(X) {
+  tmp1 <- tempfile(fileext = ".gpkg")
+  tmp2 <- tempfile(fileext = ".gpkg")
+  st_write(X, tmp1)
+  ogr2ogr(tmp1, tmp2, f = "GPKG", nlt = "MULTIPOLYGON")
+  Y <- st_read(tmp2)
+  st_sf(st_drop_geometry(X), geom = st_geometry(Y))
+}
+
+options(timeout = 3600)
 
 str <- Sys.time()
-for(i in wetfls){
-
-  fl <- basename(i)
+for(i in state.abb[29:length(state.abb)]){
 
   cat(i, '\n')
-  print(Sys.time() - str)
 
-  stateabbr <- gsub('(^.*)\\_shapefile.*$', '\\1', basename(fl))
+  ##
+  # NWI
+
+  cat('\tGet NWI data...\n')
+
+  # get file names
+  nwizip <- paste0(i, "_geodatabase_wetlands.zip")
+  nwiurl <- paste0(nwibaseurl, nwizip)
+  nwigdb <- gsub('\\.zip$', '.gdb', nwizip)
+  nwishp <- paste0(i, '_Wetlands')
 
   # download file
-  tmp1 <- tempfile(fileext = ".zip")
-  download.file(url = i, destfile = tmp1)
+  download.file(url = nwiurl, destfile = here(nwizip), mode = 'wb', quiet = T)
 
   # unzip file
-  tmp2 <- tempdir()
-  utils::unzip(tmp1, exdir = tmp2)
-
-  # get the layers from the gdb
-  gdbpth <- list.files(tmp2, pattern = stateabbr, full.names = T)
-  lyrs <- st_layers(gdbpth)$name
-  lyr <- lyrs[grepl('Wetlands$', lyrs)]
+  archive_extract(nwizip)
 
   # read the wetland layer
-  wetdat <- st_read(dsn = gdbpth, layer = lyr)
+  wetdat <- st_read(dsn = nwigdb, layer = nwishp, quiet = T) %>%
+    st_zm() #%>%
+    # ensure_multipolygons()
 
-  unlink(tmp1, recursive = T)
-  unlink(tmp2, recursive = T)
+  ##
+  # NHD
 
-  # get the corresponding NHD state layer
-  nhddat <- nhd_get(state = stateabbr)
+  cat('\tGet NHD data...\n')
 
-  # get data from above, use find-nearest for wetland layer to nhddat, those greater than x are "isolated"
-  # redo by NHD?
+  # get dl url
+  state <- abbr2state(i)
+  nhdzip <- paste0("NHD_H_", state, "_State_GDB.zip")
+  nhdurl <- paste0(nhdbaseurl, "GDB/", nhdzip)
+  nhdgdb <- gsub('\\.zip$', '.gdb', nhdzip)
+
+  # download file
+  download.file(url = nhdurl, destfile = here(nhdzip), mode = 'wb', quiet = T)
+
+  # unzip file
+  archive_extract(nhdzip)
+
+  # get flowline
+  flodat <- st_read(nhdgdb, layer = 'NHDFlowline', quiet = T)
+
+  # get waterbody
+  wbddat <- st_read(nhdgdb, layer = 'NHDWaterBody', quiet = T)
+
+  # combine flowline and waterbody
+  nhddat <- bind_rows(flodat, wbddat) %>%
+    st_zm() # %>%
+    # ensure_multipolygons()
+
+  ##
+  # get nearest and distance
+
+  cat('\tGet wetland distance to NHD...\n')
+
+  # transform nhddat crs to crs of wetdat
+  nhddat <- st_transform(nhddat, crs = st_crs(wetdat))
+
+  # get index of nearest nhddat features to wetdat features
+  nearest <- try({st_nearest_feature(st_geometry(wetdat), st_geometry(nhddat))}, silent = T)
+
+  if(inherits(nearest, 'try-error'))
+    next()
+
+  # add distance to nearest nhd to wet
+  neardist <- try({st_distance(st_geometry(wetdat), st_geometry(nhddat[nearest,]), by_element = T)})
+
+  if(inherits(neardist, 'try-error'))
+    next()
+
+  ##
+  # save
+
+  cat('\tSaving output...\n')
+
+  out <- wetdat %>%
+    st_set_geometry(NULL) %>%
+    select(ACRES) %>%
+    mutate(
+      neardest = neardist,
+      state = i
+    )
+
+  outnm <- paste0('wet', i)
+  outfl <- paste0(here('data'), '/', outnm, '.RData')
+  assign(outnm, out)
+  save(list = outnm, file = outfl)
+
+  ##
+  # clean up files
+
+  cat('\tRemoving files...\n')
+
+  unlink(nwizip)
+  unlink(nhdzip)
+  unlink(gsub('\\.gdb$', '.jpg', nhdgdb))
+  unlink(gsub('\\.gdb$', '.xml', nhdgdb))
+  unlink(nwigdb, recursive = T)
+  unlink(nhdgdb, recursive = T)
+  rm(list = c('flodat', 'wbddat', 'nhddat', 'wetdat'))
+
+  print(Sys.time() - str)
 
 }
